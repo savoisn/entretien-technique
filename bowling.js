e2ac37b838f5efa68c142274e7911a429ee454e7
savoisn <nicolas.savois@socgen.com>
2017-05-16 14:25:00 +0200

    initial commit

---
+++ bowling.js
@@ -0,0 +1,13 @@
+class Bowling{
+
+  constructor() {
+    let totalScore=2;
+  }
+
+  start(){
+     this.totalScore=0;
+  }
+
+}
+
+module.exports = Bowling;
---
+++ test/bowling.spec.js
@@ -0,0 +1,29 @@
+// Import chai.
+let chai = require('chai'),
+  path = require('path')
+
+// Tell chai that we'll be using the "should" style assertions.
+chai.should();
+should = chai.should();
+
+// Import the Rectangle class.
+let Bowling = require(path.join(__dirname, '..', 'bowling'));
+
+// The fat arrow (=>) syntax is a new way to define
+// functions in ES6. One feature that is different
+// from the usual "function" keyword is that the scope
+// is inherited from the parent, so there is no need to write
+//
+//   function() {
+//     ...
+//   }.bind(this)
+//
+// anymore. In this case we are not using "this" so the new
+// syntax is just used for brevity.
+describe('Bowling', () => {
+  it('should start', () => {
+    bowling = new Bowling();
+    bowling.start();
+    bowling.totalScore = 0;
+  });
+});
