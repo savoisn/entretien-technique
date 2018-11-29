fecc5f23560aac586361682c6ebc02b0be3d11bb
Nicolas SAVOIS <nicolas.savois@talan.com>
2018-11-29 09:56:43 +0100

    updated to babel 6

--- bowling.js
+++ bowling.js
@@ -1,13 +1,12 @@
 class Bowling{
 
   constructor() {
-    this.totalScore=2;
   }
 
   start(){
-     this.totalScore=0;
   }
 
 }
+
 
 module.exports = Bowling;
--- test/bowling.spec.js
+++ test/bowling.spec.js
@@ -1,31 +1,21 @@
 // Import chai.
-let chai = require('chai'),
-  path = require('path')
+import chai from 'chai';
 
-// Tell chai that we'll be using the "should" style assertions.
+  // Tell chai that we'll be using the "should" style assertions.
 chai.should();
-should = chai.should();
+
+let expect = chai.expect;
+
 
 // Import the Rectangle class.
-let Bowling = require(path.join(__dirname, '..', 'bowling'));
+import Bowling from "../bowling";
 
-// The fat arrow (=>) syntax is a new way to define
-// functions in ES6. One feature that is different
-// from the usual "function" keyword is that the scope
-// is inherited from the parent, so there is no need to write
-//
-//   function() {
-//     ...
-//   }.bind(this)
-//
-// anymore. In this case we are not using "this" so the new
-// syntax is just used for brevity.
 describe('Bowling', () => {
-  it('should start', () => {
-    bowling = new Bowling();
-    bowling.start();
-    bowling.totalScore = 0;
-    bowling.totalScore.should.equal(0);
+  
 
-  });
+  it('should display the score by frame', () => {
+    const bowling = new Bowling();
+    bowling.start();
+  })
+
 });
